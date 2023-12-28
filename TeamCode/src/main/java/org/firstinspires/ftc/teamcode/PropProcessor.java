7577131667e3817998c0de3be3599434f5a81186
Drew Krause <26584440+DNK1019@users.noreply.github.com>
2023-12-28 00:48:14 -0600

    Switch to a cropping based method and finish prop detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PropProcessor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PropProcessor.java
@@ -7,11 +7,13 @@ import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibra
 import org.firstinspires.ftc.vision.VisionProcessor;
 import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.*;
 
 
 public class PropProcessor implements VisionProcessor {
 
-	private Mat mask = Imgcodecs.imread("mask.png", Imgcodecs.IMREAD_GRAYSCALE);
+	// Left: 1, Center 2, Right: 3
+	private int propPosition = 0;
 
 	Telemetry telemetry;
 	public PropProcessor(Telemetry telemetry) {
@@ -20,18 +22,38 @@ public class PropProcessor implements VisionProcessor {
 
 	@Override
 	public void init(int width, int height, CameraCalibration calibration) {
-
 	}
 
 	@Override
 	public Object processFrame(Mat frame, long captureTimeNanos) {
-		telemetry.addData("Frame Height", frame.height());
-		telemetry.addData("Frame Width", frame.width());
-		telemetry.addData("Mask Height", mask.height());
-		telemetry.addData("Mask Width", mask.width());
+		// Directions are from camera perspective
+		Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGB2HSV);
+		Mat leftMat = new Mat(frame, new Rect(45, 205, 120, 113));
+		Mat centerMat= new Mat(frame, new Rect(281, 262, 94, 85));
+		Mat rightMat= new Mat(frame, new Rect(492, 218, 106, 103));
+
+		double leftSat = Core.mean(leftMat).val[1];
+		double centerSat = Core.mean(centerMat).val[1];
+		double rightSat = Core.mean(rightMat).val[1];
+
+		// Left: 1, Center 2, Right: 3
+		if(leftSat > rightSat && leftSat > centerSat) propPosition = 1;
+		else if(centerSat > leftSat && centerSat > rightSat) propPosition = 2;
+		else propPosition = 3;
+
+
+		telemetry.addData("Left Mean", leftSat);
+		telemetry.addData("Center Mean", centerSat);
+		telemetry.addData("Right Mean", rightSat);
+		
 		telemetry.update();
+		Imgproc.cvtColor(frame, frame, Imgproc.COLOR_HSV2RGB);
 		return null;
 
+	}
+
+	public int GetPropPosition() {
+		return propPosition;
 	}
 
 	@Override
