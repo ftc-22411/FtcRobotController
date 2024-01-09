7b22f0256ad516e197902ee16e18dd4eea352af7
Drew Krause <drewkrause2@gmail.com>
2024-01-08 23:20:06 -0600

    Add MeepMeep (path visualizer) and clean up redundant dependencies. Also started updating splines (in MeepMeep).

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -0,0 +1,42 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+
+public class MeepMeepTesting {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(12, 60, -Math.PI / 2))
+                .setTangent(0)
+                .splineToLinearHeading(new Pose2d(8.0, 35.0, Math.PI), -Math.PI)
+
+                // Place purple pixel
+                .stopAndAdd(new SleepAction(.5))
+
+                .strafeTo(new Vector2d(36.0, 31.0))
+
+                // Place yellow pixel
+                .stopAndAdd(new SleepAction(.5))
+
+                .setTangent(Math.PI / 2)
+                .splineToConstantHeading(new Vector2d(48.0, 60.0), 0.0)
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
